4c4b67a482b7ba8bfaf558a04743cd2b27e2e74c
logton68-del <logton68@gmail.com>
2026-03-22 18:31:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,33 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-protected_numbers = ["A096MT156", "X777XX77"]
+protected_numbers = set()
 alerts = []
 
 @app.get("/")
 def root():
     return {"message": "Server is working 🚀"}
 
+@app.get("/protect")
+def protect_plate(number: str):
+    protected_numbers.add(number.upper())
+    return {
+        "status": "protected_enabled",
+        "number": number.upper()
+    }
+
+@app.get("/unprotect")
+def unprotect_plate(number: str):
+    protected_numbers.discard(number.upper())
+    return {
+        "status": "protected_disabled",
+        "number": number.upper()
+    }
+
 @app.get("/check")
 def check_plate(number: str):
+    number = number.upper()
+
     if number in protected_numbers:
         alerts.append({
             "number": number,
@@ -21,12 +39,16 @@ def check_plate(number: str):
             "number": number,
             "alert": "created"
         }
-    else:
-        return {
-            "status": "not_found",
-            "number": number
-        }
+
+    return {
+        "status": "not_found",
+        "number": number
+    }
 
 @app.get("/alerts")
 def get_alerts():
     return alerts
+
+@app.get("/protected")
+def get_protected():
+    return list(protected_numbers)
